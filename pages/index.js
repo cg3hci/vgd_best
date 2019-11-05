cf6a58a6b854ddd804190ae016c2e33e75864f4a
Laerte <bardo_italiano@hotmail.it>
2019-11-05 23:40:42 +0100

    Improvements to carousel system

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,19 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 1
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 1
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1
   }
 };
+
+const placeholder = "https://via.placeholder.com/500x300";
 
 const Home = () => (
   <div>
@@ -31,13 +33,46 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Nav />
-    <Carousel responsive={responsive}>
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
+    <Carousel
+      showDots={true}
+      responsive={responsive}
+      ssr={true}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={3000}
+      partial={true}
+      itemClass="image-item"
+      containerClass="container"
+    >
+        <img className="slide_image" src={placeholder} />
+        <img className="slide_image" src={placeholder} />
+        <img className="slide_image" src={placeholder} />
+        <img className="slide_image" src={placeholder} />
     </Carousel>
-    ;
+    <style jsx global>
+      {`
+        .image-item {
+        }
+        .container {
+          width: width:100%;
+        }
+      `}
+    </style>
+    <style jsx>
+      {`
+        div{
+          text-align: center;
+        }
+        .slide_image {
+          user-drag: none;
+          user-select: none;
+          -moz-user-select: none;
+          -webkit-user-drag: none;
+          -webkit-user-select: none;
+          -ms-user-select: none;
+        }
+      `}
+    </style>
   </div>
 );
 
