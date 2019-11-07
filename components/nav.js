26e63a949dcab66d9fbad4bcd4c6e67056d98a8f
Laerte <bardo_italiano@hotmail.it>
2019-11-07 18:05:17 +0100

    Added proper nav links

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: 'https://elearning.unica.it/course/view.php?id=195', label: 'E-Learning VGD' },
+  { href: 'https://github.com/cg3hci/vgd_best', label: 'GitHub' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
